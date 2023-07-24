5c92e15d47ea1c6fb662d5897df4fd035a4ea775
aguconco <aguconco9@gmail.com>
2023-07-24 03:53:01 -0300

    agregar nueva flor

--- terapia_floral/UsuarioControl/UC_flores.cs
+++ terapia_floral/UsuarioControl/UC_flores.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
+using System.Management.Instrumentation;
 using System.Windows.Forms;
 using Guna.UI2.WinForms;
 using terapia_floral.Formularios;
@@ -11,28 +13,24 @@ namespace terapia_floral.UsuarioControl
     public partial class UC_flores : UserControl
     {
         private static string database = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+
+
+
         public UC_flores()
         {
             InitializeComponent();
-        }
-
-        private void UC_flores_Load(object sender, System.EventArgs e)
-        {
-            DetalleFlor(null);
-
             DataGridViewTextBoxColumn columnaNombre = new DataGridViewTextBoxColumn();
+            DataGridViewTextBoxColumn columnaDescripcion = new DataGridViewTextBoxColumn();
+            DataGridViewTextBoxColumn columnaEquivalente = new DataGridViewTextBoxColumn();
+
             columnaNombre.HeaderText = "Nombre";
             columnaNombre.DataPropertyName = "Nombre"; // Nombre del campo en el origen de datos
             columnaNombre.Name = "columnaNombre"; // Nombre único para la columna (opcional)
 
-            // Crear una columna para la descripción
-            DataGridViewTextBoxColumn columnaDescripcion = new DataGridViewTextBoxColumn();
             columnaDescripcion.HeaderText = "Descripción";
             columnaDescripcion.DataPropertyName = "Descripcion"; // Nombre del campo en el origen de datos
             columnaDescripcion.Name = "columnaDescripcion"; // Nombre único para la columna (opcional)
 
-            // Crear una columna para el equivalente
-            DataGridViewTextBoxColumn columnaEquivalente = new DataGridViewTextBoxColumn();
             columnaEquivalente.HeaderText = "Equivalente";
             columnaEquivalente.DataPropertyName = "Equivalente"; // Nombre del campo en el origen de datos
             columnaEquivalente.Name = "columnaEquivalente"; // Nombre único para la columna (opcional)
@@ -40,7 +38,17 @@ namespace terapia_floral.UsuarioControl
             tablaFlores.Columns.Add(columnaNombre);
             tablaFlores.Columns.Add(columnaDescripcion);
             tablaFlores.Columns.Add(columnaEquivalente);
+        }
 
+        private void UC_flores_Load(object sender, System.EventArgs e)
+        {
+            DetalleFlor(null);
+
+            obtenerFlores();
+        }
+
+        public void obtenerFlores()
+        {
             string sql = "SELECT * FROM flores";
 
             using (SQLiteConnection connection = new SQLiteConnection(database))
@@ -59,12 +67,11 @@ namespace terapia_floral.UsuarioControl
                     {
                         string nombre = reader["nombre"].ToString();
                         string descripcion = reader["descripcion"].ToString();
-                        string equivalente = reader["equivalentes"].ToString();
+                        string equivalente = reader["equivalente"].ToString();
                         string id = reader["id"].ToString();
 
                         int rowIndex = tablaFlores.Rows.Add(nombre, descripcion, equivalente);
                         tablaFlores.Rows[rowIndex].Tag = id;
-
                     }
 
                     reader.Close();
@@ -105,7 +112,7 @@ namespace terapia_floral.UsuarioControl
                     {
                         string nombre = reader["nombre"].ToString();
                         string descripcion = reader["descripcion"].ToString();
-                        string equivalente = reader["equivalentes"].ToString();
+                        string equivalente = reader["equivalente"].ToString();
 
                         TextBox TextBoxNombre = new TextBox();
                         RichTextBox richTextBoxDescripcion = new RichTextBox();
@@ -173,11 +180,13 @@ namespace terapia_floral.UsuarioControl
             
         }
 
+
         private void btn_nueva_flor_Click(object sender, EventArgs e)
         {
             nueva_flor NuevaFlor = new nueva_flor();
             NuevaFlor.TopMost = true;
             NuevaFlor.ShowDialog();
+            obtenerFlores();
         }
 
         private void buscadorFlor_TextChanged(object sender, EventArgs e)
@@ -202,13 +211,11 @@ namespace terapia_floral.UsuarioControl
                     {
                         string nombre = reader["nombre"].ToString();
                         string descripcion = reader["descripcion"].ToString();
-                        string equivalente = reader["equivalentes"].ToString();
+                        string equivalente = reader["equivalente"].ToString();
                         string id = reader["id"].ToString();
-
 
                         int rowIndex = tablaFlores.Rows.Add(nombre, descripcion, equivalente);
                         tablaFlores.Rows[rowIndex].Tag = id;
-
                     }
 
                     reader.Close();
